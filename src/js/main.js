2c999abbf49e279af78ca9045f35d58f97c73b98
Rebecca Emmanuel <rebecca.emmanuel@koi.fritz.box>
2020-04-09 17:16:18 +0200

    class names adjustment

--- src/js/main.js
+++ src/js/main.js
@@ -1,27 +1,24 @@
 function contactToggle1() {
-    let x = document.getElementById("contact1");
-    if (x.className === "contact") {
-      x.className += " contact--hidden";
-    } else {
-      x.className = "contact";
-    }
-    console.log('hallo');
+  let x = document.getElementById("contact1");
+  if (x.className === "contactToggleBox") {
+    x.className += " contactToggleBox--hidden";
+  } else {
+    x.className = "contactToggleBox ";
   }
-  function contactToggle2() {
-    let x = document.getElementById("contact2");
-    if (x.className === "contact") {
-      x.className += " contact--hidden";
-    } else {
-      x.className = "contact";
-    }
-    console.log('hallo');
+}
+function contactToggle2() {
+  let x = document.getElementById("contact2");
+  if (x.className === "contactToggleBox") {
+    x.className += " contactToggleBox--hidden";
+  } else {
+    x.className = "contactToggleBox";
   }
-  function contactToggle3() {
-    let x = document.getElementById("contact3");
-    if (x.className === "contact") {
-      x.className += " contact--hidden";
-    } else {
-      x.className = "contact";
-    }
-    console.log('hallo');
+}
+function contactToggle3() {
+  let x = document.getElementById("contact3");
+  if (x.className === "contactToggleBox") {
+    x.className += " contactToggleBox--hidden";
+  } else {
+    x.className = "contactToggleBox";
   }
+}
