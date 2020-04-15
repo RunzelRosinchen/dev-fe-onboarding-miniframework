2b6e02ac5d5b785a7a00591b140663395f35e89e
Rebecca Emmanuel <rebecca.emmanuel@koi.fritz.box>
2020-04-15 08:07:55 +0200

    event challenge with plain js

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,11 @@
 function contactBoxToggle(event) {
     let btn=event.target.getAttribute("linked-to");
     let box = document.getElementById(btn);
+    let allBoxes = document.getElementsByClassName("contactToggleBox");
+    for (let i=0; i<allBoxes.length; i++){
+      allBoxes[i].className = "contactToggleBox contactToggleBox--hidden";
+    console.log(allBoxes);
+    }
     if (box.className === "contactToggleBox") {
       box.className += " contactToggleBox--hidden";
     } else {
