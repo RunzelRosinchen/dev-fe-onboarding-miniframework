835a36b195cb2ff1fc29e66e905925afcfbceaf8
Rebecca Emmanuel <rebecca.emmanuel@koi.fritz.box>
2020-04-15 09:56:15 +0200

    close box when click twice on a button

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,17 @@
 function contactBoxToggle(event) {
-    let btn=event.target.getAttribute("linked-to");
-    let box = document.getElementById(btn);
-    let allBoxes = document.getElementsByClassName("contactToggleBox");
-    for (let i=0; i<allBoxes.length; i++){
-      allBoxes[i].className = "contactToggleBox contactToggleBox--hidden";
-    console.log(allBoxes);
+  let btn = event.target.getAttribute("linked-to");
+  let box = document.getElementById(btn);
+  let allBoxes = document.getElementsByClassName("contactToggleBox");
+  if (box.className === "contactToggleBox") {
+    for (let i = 0; i < allBoxes.length; i++) {
+      allBoxes[i].className = "contactToggleBox";
+      console.log(allBoxes);
     }
-    if (box.className === "contactToggleBox") {
-      box.className += " contactToggleBox--hidden";
-    } else {
-      box.className = "contactToggleBox";
+    box.className += " contactToggleBox--open";
+  } else {
+    for (let i = 0; i < allBoxes.length; i++) {
+      allBoxes[i].className = "contactToggleBox";
+      console.log(allBoxes);
     }
+  }
 }
