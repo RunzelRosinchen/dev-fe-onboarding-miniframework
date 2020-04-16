14686bc6eb26cbc7285c04ab76f37567c8868fb7
Rebecca Emmanuel <rebecca.emmanuel@koi.fritz.box>
2020-04-16 08:50:58 +0200

    js version with eventListeners

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,4 @@
+/* js version old
 function contactBoxToggle(event) {
   let btn = event.target.getAttribute("linked-to");
   let box = document.getElementById(btn);
@@ -5,13 +6,45 @@ function contactBoxToggle(event) {
   if (box.className === "contactToggleBox") {
     for (let i = 0; i < allBoxes.length; i++) {
       allBoxes[i].className = "contactToggleBox";
-      console.log(allBoxes);
     }
     box.className += " contactToggleBox--open";
   } else {
     for (let i = 0; i < allBoxes.length; i++) {
       allBoxes[i].className = "contactToggleBox";
-      console.log(allBoxes);
     }
   }
+}*/
+
+/* js version */
+
+function contactBoxToggle(event) {
+  let closeEvent = new CustomEvent("close");
+  let allBoxes = [].slice.call(
+    document.querySelectorAll(".mainConversion__contactToggleBox")
+  );
+  let box = [].slice.call(
+    event.target
+      .closest(".mainConversion")
+      .getElementsByClassName("mainConversion__contactToggleBox")
+  );
+  allBoxes = allBoxes.filter(function (item) {
+    return !box.includes(item);
+  });
+  allBoxes.forEach((boxes) => {
+    boxes.addEventListener("close", function () {
+      boxes.className = "mainConversion__contactToggleBox";
+    });
+    boxes.dispatchEvent(closeEvent);
+  });
+  if (box[0].className === "mainConversion__contactToggleBox") {
+    box[0].classList.add("mainConversion__contactToggleBox--open");
+  } else {
+    box[0].classList.remove("mainConversion__contactToggleBox--open");
+  }
 }
+
+/* jQuery version
+$("a.mainConversion__btn").click(function(){
+  $("div.contactToggleBox").toggle();
+});
+*/
